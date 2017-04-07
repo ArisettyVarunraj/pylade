582579a2ab638cc25d71bb05e72f925b3a4e7209
Pierpaolo Pantone <24alsecondo@gmail.com>
2017-04-07 17:52:25 +0200

    Generated gh-pages for commit 85ba6f1 Author: Pierpaolo Pantone <24alsecondo@gmail.com>
        Add Sphinx documentation sources

---
+++ update_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+GH_PAGES_SOURCES=docs
+
+# git checkout gh-pages
+rm -rf build _sources _static
+git checkout master ${GH_PAGES_SOURCES}
+git reset HEAD
+make -C docs/ html
+mv -fv docs/build/html/* ./
+rm -rf ${GH_PAGES_SOURCES}
+touch .nojekyll
+# git add -A
+# git ci -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
