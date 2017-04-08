af704939e0565d8fb689c621859000575239d407
Pierpaolo Pantone <24alsecondo@gmail.com>
2017-04-08 18:49:53 +0200

    Improve update_docs.sh scripts

--- update_docs.sh
+++ update_docs.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
+
+# Folders and files needed to generate Sphinx docs. These files will
+# be copied from the master branch.
 GH_PAGES_SOURCES='docs pylade'
 
-# git checkout gh-pages
-rm -rf build _sources _static
+# Copy docs source files from the master branch
 git checkout master ${GH_PAGES_SOURCES}
-git reset HEAD
+
+# Generate new HTML docs using Sphinx Makefile
 make -C docs/ html
+
+# Move generated HTML files to the main folder and remove useless folders
 mv -fv docs/build/html/* ./
 rm -rf ${GH_PAGES_SOURCES}
+
+# Create a .nojekyll file if not present
 touch .nojekyll
-git add -A
-git ci -m "Generated gh-pages for `git log master -1 --format='%B (%H)'`"
-git push origin gh-pages
+
+# Ask for confirmation before pushing
+printf "\nCommit name: '`git log master -1 --format=%B`'\n"
+read -n 1 -p "Do you want to commit and push on gh-pages? [Y/n] " answer
+
+if [[ $answer == "Y" || $answer == "y" || $answer = "" ]]; then
+  printf "\n"
+  # Stage changes, create commit and push
+  git add -A
+  git ci -m "Generated gh-pages for `git log master -1 --format='%B (%H)'`"
+  git push origin gh-pages
+else
+  printf "\nNot pushing.\n"
+fi
+
+# Go back to master branch
 # git checkout master
